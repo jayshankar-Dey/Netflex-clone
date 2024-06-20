aae9fa4d395d164aa3613112851657a52a429bae
jayshankar-Dey <157572407+jayshankar-Dey@users.noreply.github.com>
2024-06-20 18:28:27 +0530

    html and css project

---
+++ Netflix/js/home.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    autoplay: {
+        delay: 2500
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+});
